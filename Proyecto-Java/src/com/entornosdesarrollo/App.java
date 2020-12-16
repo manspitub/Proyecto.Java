00ad62247caf389c0f061ca5d2df40d2af069c76
Manuel Spinola Tubío <spinola.tuman21@triana.salesianos.edu>
2020-12-16 11:00:08 +0100

    El pepe

---
+++ Proyecto-Java/src/com/entornosdesarrollo/App.java
@@ -0,0 +1,14 @@
+package com.entornosdesarrollo;
+
+public class App {
+
+	public static void main(String[] args) {
+		System.out.println("Hola Mundo!");
+
+	}
+	
+	public static void String leerStringPorConsola () {
+		Scanner sc = new Scanner(System.in);
+	}
+
+}
